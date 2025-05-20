36b41784f333a1e75777f3ba758e164e6a9d76b6
ankathi_sathwika <ankathi_sathwika@epam.com>
2025-05-20 10:59:34 +0530

    Initial commit: add Cucumber-Allure project

---
+++ src/test/java/org/epam/runners/TestRunner.java
@@ -0,0 +1,24 @@
+package org.epam.runners;
+
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"org.epam.stepDefinitions", "org.epam.hooks"},
+        plugin = {"pretty",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "html:target/cucumber-html-report" },
+        monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
---
+++ src/test/java/org/epam/stepDefinitions/SearchSteps.java
@@ -0,0 +1,50 @@
+package org.epam.stepDefinitions;
+
+import io.cucumber.java.en.*;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.testng.Assert.assertTrue;
+
+public class SearchSteps {
+
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    String keyword;
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    @Given("I open DuckDuckGo search engine")
+    public void openDuckDuckGo() {
+        driver.set(new ChromeDriver());
+        getDriver().get("https://duckduckgo.com");
+    }
+
+    @Step("Searching for: {keyword}")
+    @When("I search for {string}")
+    public void searchForKeyword(String keyword) {
+        this.keyword = keyword;
+        WebElement searchBox = getDriver().findElement(By.name("q"));
+        searchBox.sendKeys(keyword);
+        searchBox.submit();
+    }
+
+    @Step("Verifying results contain: {keyword}")
+    @Then("I should see results related to {string}")
+    public void verifyResults(String keyword) {
+        String pageText = getDriver().getPageSource().toLowerCase();
+        savePageSource(pageText);
+        assertTrue(pageText.contains(keyword.toLowerCase()), "Keyword not found in search results!");
+        getDriver().quit();
+    }
+
+    @Attachment(value = "Page Source", type = "text/html")
+    public String savePageSource(String html) {
+        return html;
+    }
+}
